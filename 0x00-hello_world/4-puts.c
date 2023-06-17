0cb4dfbe30f18e3d53086d55d80a8c8f6881f8b1
Eng. Ahmed Gamal <eng.ahmedgamal52@gmail.com>
2023-06-17 08:46:24 -0700

    First things in C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
-
-int main(void) {
-    puts("Programming is like building a multilingual puzzle");
-    return 0;
+/**
+ * main - Entry point for the program
+ *
+ * This function is the entry point for the program.
+ * data structures or variables, executes the main logic of the program.
+ * returns an exit status to the operating system.
+ *
+ * Return: 0 on success, non-zero on failure
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
