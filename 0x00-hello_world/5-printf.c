88b888f9792df0c1b396919e74944abb52c57ce3
Eng. Ahmed Gamal <eng.ahmedgamal52@gmail.com>
2023-06-17 08:41:18 -0700

    First things in C

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,13 @@
 #include <stdio.h>
+/**
+ * main - Entry point for the program
+ *
+ * This function is the entry point for the program. It initializes any necessary
+ * data structures or variables, executes the main logic of the program, and
+ * returns an exit status to the operating system.
+ *
+ * Return: 0 on success, non-zero on failure
+ */
 int main(void)
 {printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0); }
