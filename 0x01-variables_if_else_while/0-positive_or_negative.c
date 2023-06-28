ea41e0b1d8f75180daf8c7aa62c812afdde9a685
Eng. Ahmed Gamal <eng.ahmedgamal52@gmail.com>
2023-06-28 12:33:06 -0700

    First things in C

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -18,17 +18,18 @@ int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 /* your code goes there */
-if (n>0)
+printf("%d", n)
+if (n > 0)
 {
-printf("%d is positive", n);	
+printf("is positive");	
 }
 else if (n == 0)
 {
-printf("%d is zero", n);
+printf("is zero");
 }
 else
 {
-printf("%d is negative", n);
+printf("is negative");
 }
 return (0);
 }
