7b7063e895fa1bcade56c2691b2e1c216bf521d1
Eng. Ahmed Gamal <eng.ahmedgamal52@gmail.com>
2023-06-28 13:04:56 -0700

    First things in C

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -27,21 +27,6 @@ abs_n = abs(n);
 last_digit = abs_n % 10;
 /* Print the last digit of n */
 printf("%d ", last_digit);
-/* Print a message indicating whether the last digit is >5, <6 and not 0, or 0 */
-if (last_digit > 5)
-{
-printf("and is greater than 5\n");
-}
-else if (last_digit == 0)
-{
-printf("and is 0\n");
-}
-else
-{
-printf("and is less than 6 and not 0\n");
-}
-/* Copy the output to the clipboard */
-printf("Last digit of %d is %d ", n, last_digit);
 if (last_digit > 5)
 {
 printf("and is greater than 5\n");
